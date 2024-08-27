651d366c42ebbf0ddc081c582fa17adc8f6c300a
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-08-26 22:57:45 -0400

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import "./styles.css";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import "./styles.css";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="container">
 				<Header />
-				<MoviesGrid />
+				<Router>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/watchlist">Watchlist</Link>
+							</li>
+						</ul>
+					</nav>
+					<Routes>
+						<Route path="/" element={<MoviesGrid />} />
+						<Route path="/watchlist" element={<Watchlist />} />
+					</Routes>
+				</Router>
 			</div>
 
 			<Footer />
---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,9 @@
+import "../styles.css";
+
+export default function Watchlist() {
+	return (
+		<div>
+			<h1 className="title">Your Watchlist</h1>
+		</div>
+	);
+}
