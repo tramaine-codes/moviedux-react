19ee009bcbf54c5157f61b8d708c990062c28eb8
Tramaine G. <github@tramaine.dev>
2026-03-28 20:39:47 -0400

    chore: upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
